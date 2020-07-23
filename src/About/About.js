3541d4a70c8ffcd9727955d01865d5eca6f98df9
Mmatyulkov <65899783+Mmatyulkov@users.noreply.github.com>
2020-07-23 14:48:07 +0300

    Add files via upload

--- src/About/About.js
+++ src/About/About.js
@@ -3,20 +3,25 @@ import './About.css';
 
 function About() {
   return (
-    <div className="container">
-      <div className="tab2"></div>
-      <div className="tab1"></div>
-      <div className="tab3"></div>
-      <div className="about">
-        <header className="">
-         <h1>Mihhail Matyulkov</h1>
-         <h2>Web Developer</h2>
-         <p>A very good and cheerful person. Temper extroverted. Not married.</p>
-        </header>
-        <img></img>
-      </div>
-      <div className="statistics">
-        <p>stast</p>
+    <div className="main">
+      <div className="first-page"></div>
+      <div className="container">
+        <div className="about">
+          <header className="about-container">
+          <h1>Mihhail Matyulkov</h1>
+          <h2>Web Developer</h2>
+          <p>A very good and cheerful person. Temper extroverted. Not married.</p>
+          </header>
+          <div className="img"></div>
+        </div>
+        <div className="statistics">
+          <table>
+            <tbody></tbody>
+            <thead>
+            </thead>
+          </table>
+          <p>statistics</p>
+        </div>
       </div>
     </div>
   );
--- src/LogIn/LogIn.js
+++ src/LogIn/LogIn.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import './App.css';
 
+
 function LogIn() {
     return (
       <div className="App">
-        <form className="auth-form">
-          <header className="auth-form-header"><label>Log In</label></header>
-          <div className="auth-form__controls">
-            <input type="email" className="control fail" placeholder="Email"></input>
-            <input type="password" className="control fail" placeholder="Password"></input>
+        <form className="auth-form" id="auth-form">
+          <header className="auth-form-header" id="header"><label>Personal File</label></header>
+          <div className="auth-form__controls" id="controls">
+            <input type="email" className="control fail" 
+            name="email" id="email" placeholder="Email" autoComplete="off"></input>
+            <input type="password" className="control fail" 
+            name="password" id="password" placeholder="Password"></input>
             <a href=''>Forgot password?</a>
-            <button type="submit">Sign in</button>
+            <Submit></Submit>
           </div>
         </form>
+        <div className="second-page" id="second-page"></div>
       </div>
     );
-}
+  }
 
 export default LogIn;
+
+function Submit(){
+  function mes(){
+    console.log('do not working')
+    var authform = document.getElementById("auth-form");
+    authform.classList.add('flip');
+    var secondpage = document.getElementById("second-page");
+    secondpage.classList.add('page');
+    var clear = document.getElementById("controls");
+    clear.classList.add('clear');
+    var clear = document.getElementById("header");
+    clear.classList.add('clear');
+    setTimeout(function(){
+      window.location.href = 'about';
+    }, 2 * 1000);
+  }   
+  return (
+    <button type="button" onClick={mes}>Sign in</button>
+    )
+}
